30a16355770887c488413427d8175e7b6a7e612b
Alex <asharrop@email.neit.edu>
2017-02-13 10:19:31 -0500

    addUser.php refactor and documentation

--- scripts/addUser.php
+++ scripts/addUser.php
@@ -23,20 +23,21 @@ try {
 	$stmt->bindParam(':fname', $FirstName);
 	$stmt->bindParam(':lname', $LastName);
 
-	//This is an email check to see if a user already exists with the specified username
-	foreach ($result as $found) {
-
-		if ($found["Email"] == $_POST["userName"]) {
-
-			echo "This email address is already registered to an account. ";
-		}
-	}
+	
+	
 
 	//This is a password check so the user enters the correct password
 	if ($_POST["password"] != $_POST["passConfirm"]) {
 
 		echo "The passwords do not match!";
 	}
+	//This is an email check to see if a user already exists with the specified username
+	else if ($result == $Email) {
+
+		echo "This email address is already registered to an account. ";
+	}
+	//This occurs if the above statements are not true
+	//the email cannot match an existing email and the passwords must match on register.php
 	else {
 
 		$Password = password_hash($_POST["password"], PASSWORD_DEFAULT);
