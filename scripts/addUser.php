6e3717016479b3b48549183a1bd9d0ee18785d75
Alex <asharrop@email.neit.edu>
2017-02-13 10:02:51 -0500

    Validation and Registration Fixed

--- scripts/addUser.php
+++ scripts/addUser.php
@@ -12,6 +12,10 @@ try {
 	$Password = $_POST["password"];
 	$PassConfirm = $_POST["passConfirm"];
 	
+	$emailCheck = $db->prepare("SELECT * FROM users WHERE (Email = :email)");
+	$emailCheck->bindParam(':email', $Email);
+	$emailCheck->execute();
+	$result = $emailCheck->fetchAll();
 
 	$stmt = $db->prepare("INSERT INTO users (Email, P4WD, FirstName, LastName) VALUES (:email, :pass, :fname, :lname)");
 	$stmt->bindParam(':email', $Email);
@@ -19,13 +23,10 @@ try {
 	$stmt->bindParam(':fname', $FirstName);
 	$stmt->bindParam(':lname', $LastName);
 
-	
-	
-
 	//This is an email check to see if a user already exists with the specified username
-	foreach ($result as $email) {
+	foreach ($result as $found) {
 
-		if ($email["Email"] == $_POST["userName"]) {
+		if ($found["Email"] == $_POST["userName"]) {
 
 			echo "This email address is already registered to an account. ";
 		}
@@ -34,18 +35,21 @@ try {
 	//This is a password check so the user enters the correct password
 	if ($_POST["password"] != $_POST["passConfirm"]) {
 
-	$Password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-	$stmt->execute();
+		echo "The passwords do not match!";
+	}
+	else {
 
-	echo "Successfully Registered!";
-	
-	
-	//redirect user back to homepage
-	header('Location: index.php');
+		$Password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+		$stmt->execute();
+
+		echo "Successfully Registered!";
+
+		//redirect user back to homepage
+		header('Location: index.php');
 	}
 }
 catch (PDOException $e) {
 	
-	die('Registration Failed! ');
+	die('Failed Query: ' . $e->getMessage(););
 }
 ?>
