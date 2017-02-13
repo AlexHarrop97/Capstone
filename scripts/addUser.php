4ec44ff9ee6e6e9be59b9d5b0c48f12fa1bd6e1a
Alex <asharrop@email.neit.edu>
2017-02-13 10:32:23 -0500

    More Refactoring

--- scripts/addUser.php
+++ scripts/addUser.php
@@ -38,6 +38,7 @@ try {
 		echo "The passwords do not match!";
 	}
 <<<<<<< HEAD
+<<<<<<< HEAD
 	//This is an email check to see if a user already exists with the specified username
 	else if ( !emailCheck($results, $Email) == false ) {
 
@@ -45,6 +46,8 @@ try {
 	}
 	//This occurs if the above statements are not true
 	//the email cannot match an existing email and the passwords must match on register.php
+=======
+>>>>>>> parent of 30a1635... addUser.php refactor and documentation
 =======
 >>>>>>> parent of 30a1635... addUser.php refactor and documentation
 	else {
