7c1b86cace95f7a4448e3315b0386a30413b964d
Nathan Stone <ndstone@email.neit.edu>
2017-02-13 09:28:41 -0500

    logs in session, entry to profile page, todo

---
+++ redo/profile.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+require_once('db.php');
+
+
+try{
+
+    $Email = $_SESSION['Email'];
+    $Password = $_SESSION['Password'];
+
+    echo $Email;
+    //Select all using user and password textboxes on login.php
+    $stmt = $db->prepare('SELECT * FROM users WHERE Email=:email');
+    $stmt->bindParam(':email', $Email);
+    $stmt->execute();
+    $users = $stmt->fetchAll();
+
+    foreach ($users as $user) {
+
+        if (password_verify($Password, $user["P4WD"])) {
+
+            $UserID = $user["User_ID"];
+            $UserEmail = $user["Email"];
+            $UserFName = $user["FirstName"];
+            $UserLName = $user["LastName"];
+
+
+            ?>
+           <h2>Welcome to your profile page, <?php echo $UserFName." ".$UserLName; ?></h2>
+            <?php
+        }
+
+        else {
+            header('Location: login.php');
+        }
+    }
+}
+catch (PDOException $e) {
+
+    die('Sign In Failed! ');
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Todo List</title>
+    <script type="text/javascript"
+            src="http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+</head>
+<style type="text/css">
+    ul {
+        list-style-type: none;
+    }
+
+</style>
+
+<body>
+<div id="list">
+    <h1>To-Do List</h1>
+    <?php if(!empty($items)): ?>
+        <ul>
+            <li><span class="item<?php echo $item['Status'] ? ' Status' : '' ?>"><?php echo $item['Description'];?></span>
+                <?php if(!$item['Status']): ?>
+                <a href="done.php?as=status&item=<?php echo $item['id']; ?>" class="mark-done">
+                    <?php endif; ?>
+            </li>
+        </ul>
+    <?php endif; ?>
+    <form action="add" method="post">
+        <input type="text" name="add" placeholder="Add a new task" class="input" autocomplete="off" action="add.php">
+        <input type="submit" value="Add" class="submit">
+    </form>
+</div>
+</body>
