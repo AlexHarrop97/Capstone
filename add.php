2c851325dc7033a13bcb2546e3d437bde5123b8e
Brendan McNamara <Brendancmcnamara@gmail.com>
2017-02-27 08:21:45 -0500

    Revert "Screw Up Fix"
    This reverts commit 8945658f2295ebfbf8657eb514d84713a07aebfc.

--- add.php
+++ add.php
@@ -33,4 +33,29 @@ if(isset($_POST['User_ID'])) {
 	}
 }
 header('Location: todo.php');
+
+<?php
+require_once('db.php');
+require_once('todo.php');
+
+if(isset($_POST['User_ID'])) {
+	$userid = trim($_POST['User_ID']);
+
+	if (!empty($userid)) {
+		$addedQuery = $db->prepare("
+				INSERT INTO todo(userid, description, status)
+				VALUES (:User_ID, :Description, 0())
+			");
+		$addedQuery->bindparam(':User_ID', $User_ID);
+		$addedQuery->bindparam(':Description', $Description);
+		$addedQuery->bindparam(':Status', $Status);
+
+		$addedQuery->execute([
+			'userid' = $_SESSION['User_ID'],
+			'description' = $Description,
+
+			]);
+	}
+}
+header('Location: todo.php');
 ?>
--- scripts/validateUser.php
+++ scripts/validateUser.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 // This file completes the login, users are sent here from the login page
@@ -50,3 +51,54 @@ catch (PDOException $e) {
 
 
 	
+=======
+<?php
+
+// This file completes the login, users are sent here from the login page
+// login.php is the default page of the website.
+
+require_once('../dependencies/db.php');
+
+try {
+
+	// Select all using user and password textboxes on login.php
+	$stmt = $db->prepare("SELECT * FROM users");
+
+	$stmt->execute();
+	$users = $stmt->fetchAll();
+	
+	// start the session, set the session variable for User
+	// then redirect back to index.php
+	session_start();
+
+	// check the user to see if it exists within the database
+	foreach ($users as $user) {
+
+					
+		// if the user does not exists(if the email in the db is null)
+		// send the user back to login.php
+		if ($user['Email'] != $_POST["email"] || !password_verify($_POST["password"], $user["P4WD"])) {
+
+			header('url: ../login.php?loginSuccess=0');
+		}
+		
+		else {
+			// if the user exists(checking a valid email and password combo)
+			// set a session variable and send the user to index.php
+			$_SESSION["User"] = $user["Email"];
+			echo $_SESSION["User"];
+
+			//redirect user back to homepage
+			header('Location: ../index.php');
+		}
+	}
+}
+catch (PDOException $e) {
+	
+	die('Sign In Failed! ');
+}
+?>
+
+
+	
+>>>>>>> 35519da983aaf91d4bd283179e2f634f3e51daa7
