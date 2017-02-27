03ec6e250380307bdb734760e7f7cec7204f1f41
Brendan McNamara <Brendancmcnamara@gmail.com>
2017-02-27 09:39:24 -0500

    Fix Duplication On done.php

--- done.php
+++ done.php
@@ -26,31 +26,5 @@ if(isset($_GET['as'], $_GET['item'])) {
 header('Location: todo.php');
 
 
-<?php 
-require_once("db.php");
-
-if(isset($_GET['as'], $_GET['item'])) {
-	$as = $_GET['as'];
-	$item = $_GET['item'];
-
-	switch ($as) {
-		case 'done':
-			$doneQuery = $db->prepare("
-				UPDATE items
-				SET Status = 1
-				WHERE id = :item
-				AND user= :user
-				");
-			break;
-
-			$doneQuery->execute([
-				'item' => $item, 
-				'user' => $_SESSION['User_ID']
-			]);
-		break;
-	}
-}
-
-header('Location: todo.php');
 
 ?>
