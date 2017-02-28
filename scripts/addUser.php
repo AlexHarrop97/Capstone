8f16c4ab3f1eb5ad8e51239de8188c3e1b9cf46e
Nathan Stone <ndstone@email.neit.edu>
2017-02-28 10:43:03 -0500

    Updated scripts to match change to profile.php

--- scripts/addUser.php
+++ scripts/addUser.php
@@ -2,7 +2,7 @@
 // this is where the user is created
 // the information is obtained from the register_user.php file using post method
 
-require_once('../dependencies/db.php');
+require_once('../db.php');
 
 try {
 	
@@ -12,10 +12,6 @@ try {
 	$Password = $_POST["password"];
 	$PassConfirm = $_POST["passConfirm"];
 	
-	$emailCheck = $db->prepare("SELECT * FROM users WHERE (Email = :email)");
-	$emailCheck->bindParam(':email', $Email);
-	$emailCheck->execute();
-	$result = $emailCheck->fetchAll();
 
 	$stmt = $db->prepare("INSERT INTO users (Email, P4WD, FirstName, LastName) VALUES (:email, :pass, :fname, :lname)");
 	$stmt->bindParam(':email', $Email);
@@ -23,58 +19,17 @@ try {
 	$stmt->bindParam(':fname', $FirstName);
 	$stmt->bindParam(':lname', $LastName);
 
-	//This is an email check to see if a user already exists with the specified username
-	foreach ($result as $found) {
+	$Password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+	$stmt->execute();
 
-		if ($found["Email"] == $_POST["userName"]) {
-
-			echo "This email address is already registered to an account. ";
-		}
-	}
-
-	//This is a password check so the user enters the correct password
-	if ($_POST["password"] != $_POST["passConfirm"]) {
-
-		echo "The passwords do not match!";
-	}
-
-	//This is an email check to see if a user already exists with the specified username
-	else if ( !emailCheck($result, $Email) == false ) {
-
-		echo "This email address is already registered to an account. ";
-	}
-	//This occurs if the above statements are not true
-	//the email cannot match an existing email and the passwords must match on register.php
-	else {
-
-		$Password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-		$stmt->execute();
-		
-		echo "Successfully Registered!";
-
-		//redirect user back to homepage
-		//header('Location: ../index.php');
-	}
+	echo "Successfully Registered!";
+	
+	
+	//redirect user back to homepage
+	//header('Location: index.php');
 }
 catch (PDOException $e) {
 	
-	die('Failed Query: ' . $e->getMessage());
-}
-
-
-
-function emailCheck($fetchResults, $emailIn) {
-
-	foreach($fetchResults as $found) {
-
-		if($found["Email"] == $emailIn) {
-
-			return false;
-		}
-		else {
-
-			return true;
-		}
-	}
+	die('Registration Failed! ');
 }
 ?>
